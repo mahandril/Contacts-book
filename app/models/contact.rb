be6521ef11a517bfe018f769724ef1e12870359e
mahandril <er.mahandril@gmail.com>
2014-06-20 14:01:09 -0400

    included validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ActiveRecord::Base
+#validating form fields for the presence, uniqueness and length
+validates :FirstName, presence: true, length: {minimum: 2, maximum: 25 }
+validates :LastName, presence: true, length: {minimum: 2, maximum: 25 }
+validates :EmailId, presence: true, uniqueness: true
+validates :MobileNumber, presence: true, uniqueness: true, length: {minimum: 10, maximum: 10 }
+validates :Address, presence: true, length: {minimum: 2, maximum: 25 }
+
 end
